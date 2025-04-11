00f5942d1cde1c245b1f53782dd8fbfa2104935f
K. Alex Mills <k.alex.mills@gmail.com>
2025-04-11 14:52:48 -0500

    refactor: tweaks in queue package

---
+++ pkg/fifo/ringbuf.go
@@ -0,0 +1,140 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Evan Huus
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+// NOTICE: file copied from github.com/eapache/queue/v2 -- modifications have been made.
+
+/*
+Package fifo provides a fast, ring-buffer queue based on the version suggested by Dariusz Górecki.
+Using this instead of other, simpler, queue implementations (slice+append or linked list) provides
+substantial memory and time benefits, and fewer GC pauses.
+
+The queue implemented here is as fast as it is for an additional reason: it is *not* thread-safe.
+*/
+
+package fifo
+
+// minRingbufLen is smallest capacity that a ringbuf may have.
+// Must be power of 2 for bitwise modulus: x % n == x & (n - 1).
+const minRingbufLen = 1024
+
+// ringbuf represents a single instance of the ringbuf data structure.
+type ringbuf[V any] struct {
+	buf               []*V
+	head, tail, count int
+}
+
+// newRingBuf constructs and returns a new ringbuf.
+func newRingBuf[V any](size int) *ringbuf[V] {
+	return &ringbuf[V]{
+		buf: make([]*V, max(minRingbufLen, size)),
+	}
+}
+
+// Reset resets this ring buffer without reallocating any new memory.
+func (q *ringbuf[V]) Reset() {
+	q.head = 0
+	q.tail = 0
+	q.count = 0
+}
+
+// Len returns the number of elements currently stored in the ringbuf.
+func (q *ringbuf[V]) Len() int {
+	return q.count
+}
+
+func (q *ringbuf[V]) Cap() int {
+	return cap(q.buf)
+}
+
+// resizes the ringbuf to fit exactly twice its current contents
+// this can result in shrinking if the ringbuf is less than half-full
+func (q *ringbuf[V]) resize() {
+	newBuf := make([]*V, q.count<<1)
+
+	if q.tail > q.head {
+		copy(newBuf, q.buf[q.head:q.tail])
+	} else {
+		n := copy(newBuf, q.buf[q.head:])
+		copy(newBuf[n:], q.buf[:q.tail])
+	}
+
+	q.head = 0
+	q.tail = q.count
+	q.buf = newBuf
+}
+
+// Add puts an element on the end of the ringbuf.
+func (q *ringbuf[V]) Add(elem V) {
+	if q.count == len(q.buf) {
+		q.resize()
+	}
+
+	q.buf[q.tail] = &elem
+	// bitwise modulus
+	q.tail = (q.tail + 1) & (len(q.buf) - 1)
+	q.count++
+}
+
+// Peek returns the element at the head of the ringbuf. This call panics
+// if the ringbuf is empty.
+func (q *ringbuf[V]) Peek() V {
+	if q.count <= 0 {
+		panic("ringbuf: Peek() called on empty ringbuf")
+	}
+	return *(q.buf[q.head])
+}
+
+// Get returns the element at index i in the ringbuf. If the index is
+// invalid, the call will panic. This method accepts both positive and
+// negative index values. Index 0 refers to the first element, and
+// index -1 refers to the last.
+func (q *ringbuf[V]) Get(i int) V {
+	// If indexing backwards, convert to positive index.
+	if i < 0 {
+		i += q.count
+	}
+	if i < 0 || i >= q.count {
+		panic("ringbuf: Get() called with index out of range")
+	}
+	// bitwise modulus
+	return *(q.buf[(q.head+i)&(len(q.buf)-1)])
+}
+
+// Remove removes and returns the element from the front of the ringbuf. If the
+// ringbuf is empty, the call will panic.
+func (q *ringbuf[V]) Remove() V {
+	if q.count <= 0 {
+		panic("ringbuf: Remove() called on empty ringbuf")
+	}
+	ret := q.buf[q.head]
+	q.buf[q.head] = nil
+	// bitwise modulus
+	q.head = (q.head + 1) & (len(q.buf) - 1)
+	q.count--
+	// Resize down if buffer 1/4 full.
+	if len(q.buf) > minRingbufLen && (q.count<<2) == len(q.buf) {
+		q.resize()
+	}
+	return *ret
+}
---
+++ pkg/fifo/ringbuf_test.go
@@ -0,0 +1,207 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Evan Huus
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+NOTE: This file copied from github.com/eapache/queue/v2 -- modifications have been made.
+*/
+
+package fifo
+
+import "testing"
+
+func TestQueueSimple(t *testing.T) {
+	q := newRingBuf[int](minRingbufLen)
+
+	for i := 0; i < minRingbufLen; i++ {
+		q.Add(i)
+	}
+	for i := 0; i < minRingbufLen; i++ {
+		if q.Peek() != i {
+			t.Error("peek", i, "had value", q.Peek())
+		}
+		x := q.Remove()
+		if x != i {
+			t.Error("remove", i, "had value", x)
+		}
+	}
+}
+
+func TestQueueWrapping(t *testing.T) {
+	q := newRingBuf[int](minRingbufLen)
+
+	for i := 0; i < minRingbufLen; i++ {
+		q.Add(i)
+	}
+	for i := 0; i < 3; i++ {
+		q.Remove()
+		q.Add(minRingbufLen + i)
+	}
+
+	for i := 0; i < minRingbufLen; i++ {
+		if q.Peek() != i+3 {
+			t.Error("peek", i, "had value", q.Peek())
+		}
+		q.Remove()
+	}
+}
+
+func TestQueueLength(t *testing.T) {
+	q := newRingBuf[int](minRingbufLen)
+
+	if q.Len() != 0 {
+		t.Error("empty queue length not 0")
+	}
+
+	for i := 0; i < 1000; i++ {
+		q.Add(i)
+		if q.Len() != i+1 {
+			t.Error("adding: queue with", i, "elements has length", q.Len())
+		}
+	}
+	for i := 0; i < 1000; i++ {
+		q.Remove()
+		if q.Len() != 1000-i-1 {
+			t.Error("removing: queue with", 1000-i-i, "elements has length", q.Len())
+		}
+	}
+}
+
+func TestQueueGet(t *testing.T) {
+	q := newRingBuf[int](minRingbufLen)
+
+	for i := 0; i < 1000; i++ {
+		q.Add(i)
+		for j := 0; j < q.Len(); j++ {
+			if q.Get(j) != j {
+				t.Errorf("index %d doesn't contain %d", j, j)
+			}
+		}
+	}
+}
+
+func TestQueueGetNegative(t *testing.T) {
+	q := newRingBuf[int](minRingbufLen)
+
+	for i := 0; i < 1000; i++ {
+		q.Add(i)
+		for j := 1; j <= q.Len(); j++ {
+			if q.Get(-j) != q.Len()-j {
+				t.Errorf("index %d doesn't contain %d", -j, q.Len()-j)
+			}
+		}
+	}
+}
+
+func TestQueueGetOutOfRangePanics(t *testing.T) {
+	q := newRingBuf[int](minRingbufLen)
+
+	q.Add(1)
+	q.Add(2)
+	q.Add(3)
+
+	assertPanics(t, "should panic when negative index", func() {
+		q.Get(-4)
+	})
+
+	assertPanics(t, "should panic when index greater than length", func() {
+		q.Get(4)
+	})
+}
+
+func TestQueuePeekOutOfRangePanics(t *testing.T) {
+	q := newRingBuf[any](minRingbufLen)
+
+	assertPanics(t, "should panic when peeking empty queue", func() {
+		q.Peek()
+	})
+
+	q.Add(1)
+	q.Remove()
+
+	assertPanics(t, "should panic when peeking emptied queue", func() {
+		q.Peek()
+	})
+}
+
+func TestQueueRemoveOutOfRangePanics(t *testing.T) {
+	q := newRingBuf[int](minRingbufLen)
+
+	assertPanics(t, "should panic when removing empty queue", func() {
+		q.Remove()
+	})
+
+	q.Add(1)
+	q.Remove()
+
+	assertPanics(t, "should panic when removing emptied queue", func() {
+		q.Remove()
+	})
+}
+
+func assertPanics(t *testing.T, name string, f func()) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("%s: didn't panic as expected", name)
+		}
+	}()
+
+	f()
+}
+
+// WARNING: Go's benchmark utility (go test -bench .) increases the number of
+// iterations until the benchmarks take a reasonable amount of time to run; memory usage
+// is *NOT* considered. On a fast CPU, these benchmarks can fill hundreds of GB of memory
+// (and then hang when they start to swap). You can manually control the number of iterations
+// with the `-benchtime` argument. Passing `-benchtime 1000000x` seems to be about right.
+
+func BenchmarkQueueSerial(b *testing.B) {
+	q := newRingBuf[any](minRingbufLen)
+	for i := 0; i < b.N; i++ {
+		q.Add(nil)
+	}
+	for i := 0; i < b.N; i++ {
+		q.Peek()
+		q.Remove()
+	}
+}
+
+func BenchmarkQueueGet(b *testing.B) {
+	q := newRingBuf[int](minRingbufLen)
+	for i := 0; i < b.N; i++ {
+		q.Add(i)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		q.Get(i)
+	}
+}
+
+func BenchmarkQueueTickTock(b *testing.B) {
+	q := newRingBuf[any](minRingbufLen)
+	for i := 0; i < b.N; i++ {
+		q.Add(nil)
+		q.Peek()
+		q.Remove()
+	}
+}
